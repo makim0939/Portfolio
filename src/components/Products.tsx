7ca77e60955de7b1a24d80a718ffd393388fdb24
makim0939 <95673492+makim0939@users.noreply.github.com>
2024-02-01 12:07:34 +0900

    スクロールの監視をFramermotionからLenisに変更

--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,41 +1,32 @@
-import { productNames, productsInfo } from "@/info";
-import Image from "next/image";
-import React, { use, useContext, useEffect, useRef } from "react";
-import { motion, useInView, useMotionValueEvent, useScroll } from "framer-motion";
-import IndexText from "./animation/IndexText";
-import { gridAnimationRequestAtom, headerModeAtom } from "@/atoms";
+import React, { useEffect, useRef } from "react";
+import { headerModeAtom } from "@/atoms";
 import { useAtom } from "jotai";
-import { EASE } from "@/animationProps";
-import { GridAppContext } from "@/app/GridProvider";
 import SoftwareProducts from "./ui/Products/SoftwareProduct";
 import ProductsIndex from "./ui/Products/ProductsIndex";
 import CgProduct from "./ui/Products/CgProduct";
+import { useLenis } from "@/providers/LenisProvider";
+
 const Products = ({ screenWidth }: { screenWidth: number }) => {
-  const gridApp = useContext(GridAppContext);
   const ref = useRef<HTMLDivElement>(null);
   const [, setHeaderMode] = useAtom(headerModeAtom);
-  const { scrollY } = useScroll();
+  const lenis = useLenis();
 
-  useMotionValueEvent(scrollY, "change", (latest) => {
+  lenis?.on("scroll", () => {
+    console.log("scroll");
     if (!ref.current) return;
-    console.log(ref.current.getBoundingClientRect().y);
+    // console.log(ref.current.getBoundingClientRect().y);
     if (ref.current.getBoundingClientRect().y < 1) {
-      ref.current.className = "";
       setHeaderMode("small");
       return;
     }
     if (ref.current.getBoundingClientRect().y > -10) {
-      ref.current.className = "h-[100vh]";
-      gridApp.setScroll();
       setHeaderMode("default");
       return;
     }
   });
 
   useEffect(() => {
-    return () => {
-      setHeaderMode("default");
-    };
+    return () => setHeaderMode("default");
   }, [setHeaderMode]);
 
   return (
