a343950400fb53775b2bae29995258fc0562c638
makim0939 <95673492+makim0939@users.noreply.github.com>
2024-02-24 00:22:37 +0900

    スキルのアイコンのサイズを可変に

--- src/components/icon/SkillsIcon.tsx
+++ src/components/icon/SkillsIcon.tsx
@@ -1,14 +1,15 @@
 import Image from "next/image";
 import React from "react";
 
-const SkillsIcon = ({ src }: { src: string }) => {
+const SkillsIcon = ({ src, size, className = "" }: { src: string; size: number; className?: string }) => {
   return (
     <Image
       src={src}
       alt=""
-      width={64}
-      height={64}
-      className=" bg-white rounded-lg shadow-lg shadow-neutral-200"
+      width={size}
+      height={size}
+      className={" p-1 bg-white rounded-lg shadow-lg shadow-neutral-200" + className}
+      style={{ width: size, height: size }}
     />
   );
 };
