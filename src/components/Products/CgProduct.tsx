8028e1b3e62ffe4714992cd4d713221e1fb4bd07
makim0939 <95673492+makim0939@users.noreply.github.com>
2024-02-24 18:32:52 +0900

    PC版での動画再生の挙動を修正

--- src/components/Products/CgProduct.tsx
+++ src/components/Products/CgProduct.tsx
@@ -4,39 +4,42 @@ import React, { useEffect } from "react";
 
 const CgProduct = ({ product, mobile = false }: { product: CgProductInfo; mobile?: boolean }) => {
   const [[width, height], setWindowSize] = React.useState([0, 0]);
-  const [loaded, setLoaded] = React.useState(false);
+  const [canplay, setCanplay] = React.useState(false);
   useEffect(() => {
     setWindowSize([window.innerWidth, window.innerHeight]);
 
-    const onVideoLoaded = () => {
-      setLoaded(true);
+    const onVideoCanplay = () => {
+      setCanplay(true);
     };
 
-    if (product.type !== "video") return;
+    if (product.type !== "video" || !mobile) return;
     const video = document.querySelector("video");
     if (!video) return;
-    video.addEventListener("loadedmetadata", onVideoLoaded);
+    video.addEventListener("canplay", onVideoCanplay);
     return () => {
-      video.removeEventListener("loadedmetadata", onVideoLoaded);
+      video.removeEventListener("canplay", onVideoCanplay);
     };
-  }, [product]);
+  }, [product, mobile]);
+
+  const loaded = canplay && mobile;
+  console.log("loaded", loaded);
 
   if (product.type === "video")
     return (
       <div className={mobile ? "p-4" : "w-[100vw]  h-[100vh] relative"}>
         {mobile && <h2 className=" w-full text-2xl font-extralight ">{product.title}</h2>}
-        {!loaded && <p>Loading ... </p>}
+        {/* {!loaded && <p>Loading ... </p>} */}
         <video
           src={product.image}
           width={width}
           height={height}
           className=" w-full h-full object-cover -z-10"
           loop
-          autoPlay
+          autoPlay={true}
           muted
           playsInline={true}
           controls={false}
-          hidden={!loaded}
+          hidden={false}
         ></video>
       </div>
     );
