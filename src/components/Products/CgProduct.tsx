53e82613161d14613074b2d29483ab790460d6a8
makim0939 <95673492+makim0939@users.noreply.github.com>
2024-02-25 09:52:08 +0900

    モバイル版のビデオの表示を修正

--- src/components/Products/CgProduct.tsx
+++ src/components/Products/CgProduct.tsx
@@ -1,44 +1,42 @@
 import { CgProductInfo } from "@/info";
 import Image from "next/image";
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 
 const CgProduct = ({ product, mobile = false }: { product: CgProductInfo; mobile?: boolean }) => {
   const [[width, height], setWindowSize] = React.useState([0, 0]);
   const [canplay, setCanplay] = React.useState(false);
+  const videoRef = useRef<HTMLVideoElement>(null);
+
   useEffect(() => {
     setWindowSize([window.innerWidth, window.innerHeight]);
 
-    const onVideoCanplay = () => {
-      setCanplay(true);
-    };
-
+    const onVideoCanplay = (e: Event) => setCanplay(true);
     if (product.type !== "video" || !mobile) return;
-    const video = document.querySelector("video");
-    if (!video) return;
+    if (!videoRef.current) return;
+    const video = videoRef.current;
+    video.load();
     video.addEventListener("canplay", onVideoCanplay);
     return () => {
       video.removeEventListener("canplay", onVideoCanplay);
     };
   }, [product, mobile]);
 
-  const loaded = canplay && mobile;
-  console.log("loaded", loaded);
-
   if (product.type === "video")
     return (
       <div className={mobile ? "p-4" : "w-[100vw]  h-[100vh] relative"}>
         {mobile && <h2 className=" w-full text-2xl font-extralight ">{product.title}</h2>}
-        {/* {!loaded && <p>Loading ... </p>} */}
+        {mobile && !canplay && <p>Loading ... </p>}
         <video
+          ref={videoRef}
           src={product.image}
           width={width}
           height={height}
           className=" w-full h-full object-cover -z-10"
           loop
-          autoPlay={true}
+          autoPlay={mobile ? false : true}
           muted
           playsInline={true}
-          controls={false}
+          controls={mobile ? true : false}
           hidden={false}
         ></video>
       </div>
